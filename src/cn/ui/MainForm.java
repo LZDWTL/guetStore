de3222629dbea5579cfa67e4c23b14934dd2d40d
LZDWTL <1023723180@qq.com>
2021-05-02 21:29:04 +0800

    增加刷新、增加、删除用户的按钮

--- src/cn/ui/MainForm.java
+++ src/cn/ui/MainForm.java
@@ -15,6 +15,8 @@ import javax.swing.table.DefaultTableModel;
  * @author 1
  */
 public class MainForm extends JFrame {
+    JDBC jdbc = new JDBC();
+
     public MainForm() {
         initComponents();
     }
@@ -30,6 +32,48 @@ public class MainForm extends JFrame {
         delBut.setVisible(true);
     }
 
+    private void addButActionPerformed(ActionEvent e) {
+        unserinfoLab.setVisible(false);
+        panel1.setVisible(true);
+        titleLab.setVisible(false);
+        userinfoscrollPane.setVisible(false);
+    }
+
+    private void flashButActionPerformed(ActionEvent e) {
+
+        unserinfoLab.setVisible(true);
+        userinfoscrollPane.setVisible(true);
+        panel1.setVisible(false);
+        DefaultTableModel tableModel = new DefaultTableModel(jdbc.selectUserInfo(), jdbc.getHead()) {
+            public boolean isCellEditable(int row, int column) {
+                return false;
+            }
+        };
+        userinfoTab.setModel(tableModel);
+    }
+
+    private void returnButtonActionPerformed(ActionEvent e) {
+        unserinfoLab.setVisible(true);
+        userinfoscrollPane.setVisible(true);
+        panel1.setVisible(false);
+    }
+
+    private void saveButtonActionPerformed(ActionEvent e) {
+        String id=idtextField.getText();
+        String name=nametextField.getText();
+        String passwd=passwdtextField.getText();
+        jdbc.addData(Integer.parseInt(id),name,passwd);
+        idtextField.setText("");
+        nametextField.setText("");
+        passwdtextField.setText("");
+    }
+
+    private void delButActionPerformed(ActionEvent e) {
+        int rownum=userinfoTab.getSelectedRow();        //获取所选中的行
+        String id =userinfoTab.getValueAt(rownum,0).toString();  //获取选中的行中的第一列
+        jdbc.delData(id);
+    }
+
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
         menuBar1 = new JMenuBar();
@@ -37,6 +81,15 @@ public class MainForm extends JFrame {
         retriveuserMenu = new JMenuItem();
         updateuserMenu = new JMenuItem();
         Tools = new JMenu();
+        panel1 = new JPanel();
+        idLabel = new JLabel();
+        idtextField = new JTextField();
+        nameLabel = new JLabel();
+        nametextField = new JTextField();
+        passwdLabel = new JLabel();
+        passwdtextField = new JTextField();
+        saveButton = new JButton();
+        returnButton = new JButton();
         titleLab = new JLabel();
         userinfoscrollPane = new JScrollPane();
         userinfoTab = new JTable();
@@ -81,11 +134,54 @@ public class MainForm extends JFrame {
         }
         setJMenuBar(menuBar1);
 
+        //======== panel1 ========
+        {
+            panel1.setLayout(null);
+
+            //---- idLabel ----
+            idLabel.setText("\u7528\u6237ID");
+            idLabel.setFont(idLabel.getFont().deriveFont(idLabel.getFont().getSize() + 5f));
+            panel1.add(idLabel);
+            idLabel.setBounds(new Rectangle(new Point(60, 40), idLabel.getPreferredSize()));
+            panel1.add(idtextField);
+            idtextField.setBounds(135, 40, 155, idtextField.getPreferredSize().height);
+
+            //---- nameLabel ----
+            nameLabel.setText("\u7528\u6237\u540d");
+            nameLabel.setFont(nameLabel.getFont().deriveFont(nameLabel.getFont().getSize() + 5f));
+            panel1.add(nameLabel);
+            nameLabel.setBounds(new Rectangle(new Point(60, 80), nameLabel.getPreferredSize()));
+            panel1.add(nametextField);
+            nametextField.setBounds(135, 80, 155, nametextField.getPreferredSize().height);
+
+            //---- passwdLabel ----
+            passwdLabel.setText("\u5bc6   \u7801");
+            passwdLabel.setFont(passwdLabel.getFont().deriveFont(passwdLabel.getFont().getSize() + 5f));
+            panel1.add(passwdLabel);
+            passwdLabel.setBounds(new Rectangle(new Point(60, 120), passwdLabel.getPreferredSize()));
+            panel1.add(passwdtextField);
+            passwdtextField.setBounds(135, 120, 155, passwdtextField.getPreferredSize().height);
+
+            //---- saveButton ----
+            saveButton.setText("\u4fdd\u5b58");
+            saveButton.addActionListener(e -> saveButtonActionPerformed(e));
+            panel1.add(saveButton);
+            saveButton.setBounds(new Rectangle(new Point(80, 175), saveButton.getPreferredSize()));
+
+            //---- returnButton ----
+            returnButton.setText("\u8fd4\u56de");
+            returnButton.addActionListener(e -> returnButtonActionPerformed(e));
+            panel1.add(returnButton);
+            returnButton.setBounds(new Rectangle(new Point(205, 175), returnButton.getPreferredSize()));
+        }
+        contentPane.add(panel1);
+        panel1.setBounds(215, 125, 360, 225);
+
         //---- titleLab ----
         titleLab.setText("\u7535\u5b50\u5546\u94fa");
         titleLab.setFont(titleLab.getFont().deriveFont(titleLab.getFont().getStyle() | Font.BOLD, titleLab.getFont().getSize() + 48f));
         contentPane.add(titleLab);
-        titleLab.setBounds(230, 190, 315, 57);
+        titleLab.setBounds(205, 180, 315, 57);
 
         //======== userinfoscrollPane ========
         {
@@ -102,11 +198,13 @@ public class MainForm extends JFrame {
 
         //---- flashBut ----
         flashBut.setText("\u5237\u65b0");
+        flashBut.addActionListener(e -> flashButActionPerformed(e));
         contentPane.add(flashBut);
         flashBut.setBounds(new Rectangle(new Point(145, 405), flashBut.getPreferredSize()));
 
         //---- addBut ----
         addBut.setText("\u589e\u52a0");
+        addBut.addActionListener(e -> addButActionPerformed(e));
         contentPane.add(addBut);
         addBut.setBounds(new Rectangle(new Point(290, 405), addBut.getPreferredSize()));
 
@@ -117,6 +215,7 @@ public class MainForm extends JFrame {
 
         //---- delBut ----
         delBut.setText("\u5220\u9664");
+        delBut.addActionListener(e -> delButActionPerformed(e));
         contentPane.add(delBut);
         delBut.setBounds(new Rectangle(new Point(580, 405), delBut.getPreferredSize()));
 
@@ -125,7 +224,7 @@ public class MainForm extends JFrame {
         setLocationRelativeTo(getOwner());
         // JFormDesigner - End of component initialization  //GEN-END:initComponents
 
-
+        panel1.setVisible(false);
         unserinfoLab.setVisible(false);
         userinfoscrollPane.setVisible(false);
         flashBut.setVisible(false);
@@ -133,7 +232,7 @@ public class MainForm extends JFrame {
         updateBut.setVisible(false);
         delBut.setVisible(false);
 
-        JDBC jdbc=new JDBC();
+
 
         /**
          * 下面这几行代码可以让JTable显示数据
@@ -154,6 +253,15 @@ public class MainForm extends JFrame {
     private JMenuItem retriveuserMenu;
     private JMenuItem updateuserMenu;
     private JMenu Tools;
+    private JPanel panel1;
+    private JLabel idLabel;
+    private JTextField idtextField;
+    private JLabel nameLabel;
+    private JTextField nametextField;
+    private JLabel passwdLabel;
+    private JTextField passwdtextField;
+    private JButton saveButton;
+    private JButton returnButton;
     private JLabel titleLab;
     private JScrollPane userinfoscrollPane;
     private JTable userinfoTab;
--- src/cn/util/JDBC.java
+++ src/cn/util/JDBC.java
@@ -12,11 +12,10 @@ import java.util.ArrayList;
  * @date 2021-04-23 20:58
  */
 public class JDBC {
-    java.util.List<Users> list=new ArrayList<Users>();     //将数据装入到list中，然后存放到Jtable
-    private String url="jdbc:oracle:thin:@120.77.80.134:1521:orcl"; //2、连接数据库
-    private Connection conn;
-    private ResultSet rs=null;
-    private Object[][] data=null;
+    java.util.List<Users> list = new ArrayList<Users>();     //将数据装入到list中，然后存放到Jtable
+
+    private ResultSet rs = null;
+    private Object[][] data = null;
     private String head[] = {"id", "username", "password"};
 
     public String[] getHead() {
@@ -27,36 +26,31 @@ public class JDBC {
         this.head = head;
     }
 
-    {
+
+    public Boolean selectUser(String username, String password) {
+        String url = "jdbc:oracle:thin:@120.77.80.134:1521:orcl"; //2、连接数据库
+        Connection conn = null;
+        Statement stmt = null;  //3、创建SQL语句对象
+        String sql = "select password from users where username='" + username + "'";
         try {
             Class.forName("oracle.jdbc.driver.OracleDriver");   //1、创建驱动
-            conn = DriverManager.getConnection(url,"scott","tiger");  //2、连接数据库
-        } catch (SQLException | ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public Boolean selectUser(String username,String password){
-        Statement stmt=null;  //3、创建SQL语句对象
-        String sql="select password from users where username='"+username+"'";
-        try{
-            stmt=conn.createStatement();
-            rs=stmt.executeQuery(sql);  //executeQuery会返回一个结果集     4、执行SQL语句
+            conn = DriverManager.getConnection(url, "scott", "tiger");  //2、连接数据库
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);  //executeQuery会返回一个结果集     4、执行SQL语句
             //rs是结果集，又称为游标，就是一个内存区（缓冲区），查询的结果都在缓冲区
             rs.next();          //5、处理结果集
-            String encoderPassword=rs.getString(1);
-            Boolean isSuccecc=MD5.checkpassword(password,encoderPassword);  //前一个数据是文本框中的，后一个数据是数据库中的
-            if(isSuccecc){
+            String encoderPassword = rs.getString(1);
+            Boolean isSuccecc = MD5.checkpassword(password, encoderPassword);  //前一个数据是文本框中的，后一个数据是数据库中的
+            if (isSuccecc) {
                 System.out.println("登陆成功!");
                 return true;
-            }
-            else{
+            } else {
                 System.out.println("登陆失败!");
                 return false;
             }
-        }catch ( SQLException | NoSuchAlgorithmException | UnsupportedEncodingException e){
+        } catch (SQLException | NoSuchAlgorithmException | UnsupportedEncodingException | ClassNotFoundException e) {
             e.printStackTrace();
-        } finally{
+        } finally {
             try {
                 rs.close();              //6、关闭链接，释放资源
                 stmt.close();
@@ -68,24 +62,28 @@ public class JDBC {
         return false;
     }
 
-    public Object[][] selectUserInfo(){
+    public Object[][] selectUserInfo() {
 
-        java.util.List<Users> list=new ArrayList<Users>();
-        Statement statement=null;
-        String sql="select * from users";
+        java.util.List<Users> list = new ArrayList<Users>();
+        String url = "jdbc:oracle:thin:@120.77.80.134:1521:orcl"; //2、连接数据库
+        Connection conn = null;
+        Statement statement = null;
+        String sql = "select * from users";
         try {
-            statement=conn.createStatement();
-            rs=statement.executeQuery(sql);
-            while(rs.next()){
-                Users users=new Users();
+            Class.forName("oracle.jdbc.driver.OracleDriver");   //1、创建驱动
+            conn = DriverManager.getConnection(url, "scott", "tiger");  //2、连接数据库
+            statement = conn.createStatement();   //3、创建SQL对象
+            rs = statement.executeQuery(sql);
+            while (rs.next()) {
+                Users users = new Users();
                 users.setId(rs.getInt(1));
                 users.setUsername(rs.getString(2));
-                users.setPassword(rs.getString(1));
+                users.setPassword(rs.getString(3));
                 list.add(users);
             }
-        } catch (SQLException e) {
+        } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
-        }finally {
+        } finally {
             try {
                 rs.close();  //关闭结果集
                 statement.close();   //关闭SQL对象
@@ -95,14 +93,69 @@ public class JDBC {
             }
         }
 
-        data =new Object[list.size()][head.length];
-        for(int i=0;i<list.size();i++){
-            for(int j =0;j<head.length;j++){
+        data = new Object[list.size()][head.length];
+        for (int i = 0; i < list.size(); i++) {
+//            for (int j = 0; j < head.length; j++) {
                 data[i][0] = list.get(i).getId();
                 data[i][1] = list.get(i).getUsername();
                 data[i][2] = list.get(i).getPassword();
-            }
+//            }
         }
         return data;
+    }
+
+    public void addData(int id, String name, String passwd){
+        String url="jdbc:oracle:thin:@120.77.80.134:1521:orcl";
+        Connection conn=null;
+        String sql="insert into users values(?,?,?)";   // ?是占位符
+        PreparedStatement pstmt=null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn=DriverManager.getConnection(url,"scott","tiger");
+            pstmt=conn.prepareStatement(sql);
+
+            pstmt.setInt(1,id);
+            pstmt.setString(2,name);
+            pstmt.setString(3,passwd);
+            pstmt.executeUpdate();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                conn.close();
+                pstmt.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void delData(String id){
+        String url="jdbc:oracle:thin:@120.77.80.134:1521:orcl";
+        Connection conn=null;
+        String sql="delete from users where id=?";
+        PreparedStatement pstmt=null;
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            conn=DriverManager.getConnection(url,"scott","tiger");
+            pstmt=conn.prepareStatement(sql);
+
+            pstmt.setInt(1,Integer.parseInt(id));
+            pstmt.executeUpdate();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                pstmt.close();
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
